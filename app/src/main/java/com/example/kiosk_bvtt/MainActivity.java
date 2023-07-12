3dba94a0d6f49c8e9ec86ec99eb694de8e424e3a
DESKTOP-IHUEG7O\Thu <thuangl20012gmail.com>
2023-07-13 00:50:33 +0700

    2

--- app/src/main/java/com/example/kiosk_bvtt/MainActivity.java
+++ app/src/main/java/com/example/kiosk_bvtt/MainActivity.java
@@ -5,26 +5,44 @@ import static android.content.ContentValues.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    int SPLASH_TIME_OUT=3000;
     WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoadingDialog loadingDialog=new LoadingDialog(MainActivity.this);
         webview = (WebView) findViewById(R.id.webview);
         webview.setWebViewClient(new WebViewClient());
         webview.clearCache(true);
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("http://192.168.31.200:8083/phongmo/phong?id=1");
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+//        loadingDialog.startLoadingDialog();
+                webview.loadUrl("https://www.youtube.com/");
+
+
+                finish();
+            }
+        },SPLASH_TIME_OUT);
         Log.i(TAG,"Exiting onCreate");
         WebSettings webSettings=webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
+
+
     @Override
     public void onBackPressed() {
         if(webview.canGoBack())
